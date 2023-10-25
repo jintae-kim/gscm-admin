ce7260ebb157bc7ea6e6b69f8991bbecab6f4dc8
woojae <ds6318@naver.com>
2023-10-25 15:33:48 +0900

    feat:A1201010000 그리드 수정

--- src/pages/Admin/ReportConfig/SCM_PRG_A1201010000.jsx
+++ src/pages/Admin/ReportConfig/SCM_PRG_A1201010000.jsx
@@ -53,88 +53,61 @@ const SCM_PRG_A1201010000 = (props) => {
 
           <div className="grid-container">
 
-            <div className="grid-section">
+            <div className="grid-twoSide-wrap">
+              <div className="grid-section">
 
-              <div className="grid-area">
+                <div className="grid-area">
 
-                <div
-                  style={{ height: "600px", background: "#ddd" }}
-                >
-                  그리드영역
+                  <div
+                    style={{ height: "600px", background: "#ddd" }}
+                  >
+                    그리드영역
+                  </div>
+
+                </div>
+
+                <div className="grid-bottom">
+                  <div className="grid-total">
+                    총 00개(현재페이지 0/전체페이지 000000)
+                  </div>
                 </div>
 
               </div>
 
-              <div className="grid-bottom">
-                <div className="grid-total">
-                  총 00개(현재페이지 0/전체페이지 000000)
+              <div className="grid-twoSide">
+                <div className="grid-section">
+
+                  <div className="grid-area">
+
+                    <div style={{ height: "300px", background: "#ddd" }}>그리드 영역</div>
+
+                  </div>
+
+                  <div className="grid-bottom">
+                    <div className="grid-total">
+                      총 00개(현재페이지 0/전체페이지 000000)
+                    </div>
+                  </div>
                 </div>
 
-                <div className="grid-buttons">
-                  <Button className="normal-button">등록</Button>
-                  <Button className="normal-button">삭제</Button>
-                  <Button className="normal-button" onClick={togglePopup}>저장</Button>
-                </div>
-              </div>
+                <div className="grid-section">
 
-            </div>
+                  <div className="grid-area">
 
-            <div className="grid-section">
+                    <div style={{ height: "300px", background: "#ddd" }}>그리드 영역</div>
 
-              <div className="grid-area">
+                  </div>
 
-                <div style={{ height: "600px", background: "#ddd" }}>그리드 영역</div>
-
-              </div>
-
-              <div className="grid-bottom">
-                <div className="grid-total">
-                  총 00개(현재페이지 0/전체페이지 000000)
-                </div>
-
-                <div className="grid-buttons">
-                  <Button className="normal-button">등록</Button>
-                  <Button className="normal-button">삭제</Button>
-                  <Button className="normal-button" onClick={togglePopup}>저장</Button>
-                </div>
+                  <div className="grid-bottom">
+                    <div className="grid-total">
+                      총 00개(현재페이지 0/전체페이지 000000)
+                    </div>
+                  </div>
+                </div>            
               </div>
             </div>
 
           </div>
-
-
-          {/* //-- 저장 모달 */}
-          <Popup
-            visible={isPopupVisible}
-            hideOnOutsideClick={true}
-            onHiding={togglePopup}
-            width={1000}
-            height={500}
-            dragEnabled={false}
-            shadingColor="rgba(0, 0, 0, 0.5)"
-          >
-            <div className="modal-header">
-              <h3 className="modal-tit">저장</h3>
-              <span className="modal-subtit">{props.title}</span>
-            </div>
-
-            <div className="modal-body">
-              <h5 className="mc-tit">
-                <span className="mct-icon"></span>저장목록(총 4개)
-              </h5>
-
-              <div className="grid-box">그리드영역</div>
-
-              <p className="mc-ques">상기 항목을 저장 하시겠습니까?</p>
-            </div>
-
-            <div className="modal-footer">
-              <Button className="cancle-btn" onClick={togglePopup}>취소</Button>
-              <Button className="confirm-btn" onClick={togglePopup}>확인</Button>
-            </div>
-
-          </Popup>
-          {/* 저장 모달 --// */}
 
 
         </div>
